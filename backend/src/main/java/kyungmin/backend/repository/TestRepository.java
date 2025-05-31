c243a81d80f71453dcba478a34beba703abc5398
Kim KyungMin <k.kyungmin2892@gmail.com>
2025-05-31 15:43:34 +0900

    add: TestRepsoitory 추가

---
+++ backend/src/main/java/kyungmin/backend/repository/TestRepository.java
@@ -0,0 +1,9 @@
+package kyungmin.backend.repository;
+
+import kyungmin.backend.domain.TestEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TestRepository extends JpaRepository<TestEntity, Long> {
+}
