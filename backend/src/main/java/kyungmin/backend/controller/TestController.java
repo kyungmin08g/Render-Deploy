8d3134971885456781af908e775bc70e4650d6a2
Kim KyungMin <k.kyungmin2892@gmail.com>
2025-05-31 15:53:30 +0900

    feat: Controller단 완료

--- backend/src/main/java/kyungmin/backend/controller/TestController.java
+++ backend/src/main/java/kyungmin/backend/controller/TestController.java
@@ -1,14 +1,18 @@
 package kyungmin.backend.controller;
 
+import jakarta.validation.Valid;
+import kyungmin.backend.controller.request.CreateTestRequest;
+import kyungmin.backend.domain.TestEntity;
+import kyungmin.backend.service.TestService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class TestController {
+  private final TestService testService;
 
   @Value("${test}")
   private String test;
@@ -16,5 +20,16 @@ public class TestController {
   @GetMapping(value = "/")
   public ResponseEntity<String> test() {
     return ResponseEntity.ok(test);
+  }
+
+  @PostMapping(value = "/test")
+  public ResponseEntity<Void> save(@RequestBody @Valid CreateTestRequest request) {
+    testService.save(request);
+    return ResponseEntity.ok().build();
+  }
+
+  @GetMapping(value = "/test/content/{id}")
+  public ResponseEntity<String> getContent(@PathVariable("id") String id) {
+    return ResponseEntity.ok(testService.getContent(Long.parseLong(id)));
   }
 }
